f6fedfd5bef9f7f82ad605ecd920630ae2f0a6d3
Hack Reactor Students <taylor.harwin@gmail.com>
2014-04-25 16:32:56 -0700

    Implemented randomly moving all enemies.

--- watchout.js
+++ watchout.js
@@ -5,20 +5,24 @@
 var svg = d3.select('body').append('svg').attr('class','gameBoard');
 
 //Create 13 enemies within SVG board. Enemies are small black circles
-
+var boardSize = [800, 800];
 var enemyCount = 10;
 var xPos = 50;
 var yPos = 50;
 var enemies = [];
 
+var randomLocations = function(boardSize){
+  xPos = Math.random() * boardSize[0];
+  yPos = Math.random() * boardSize[1];
+  return [xPos, yPos];
+};
+
 // loop from 0 to enemyCount
 for(var i = 0; i < enemyCount; i++){
-  // append new enemy with a random xPos and yPos between 0 - 900
-  xPos = Math.random() * 900;
-  yPos = Math.random() * 900;
+  var coords =  randomLocations(boardSize);
 
   var enemy = svg.append('g')
-                .attr('transform', 'translate(' + xPos + ',' + yPos + ')')
+                .attr('transform', 'translate(' + coords[0] + ',' + coords[1] + ')')
                 .append('circle')
                 .attr('cx','25')
                 .attr('cy','25')
@@ -29,3 +33,29 @@ for(var i = 0; i < enemyCount; i++){
 
   enemies.push(enemy);
 }
+
+// create function .move taking x and y for new enemy location
+// vars enemy, x, y
+var moveEnemy = function(enemy, coords, duration){
+  enemy.transition().duration(duration).attr('transform', 'translate(' + xPos + ',' + yPos + ')');
+};
+
+var moveAllEnemies = function(){
+  for (var j = 0; j < enemies.length; j++){
+    moveEnemy(enemies[j],randomLocations(boardSize), 1000);
+  }
+};
+
+var startGame = function(){
+  setInterval(function(){
+    moveAllEnemies();
+  }, 1000);
+};
+
+startGame();
+
+
+
+
+
+
