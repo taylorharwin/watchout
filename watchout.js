048457f59af1f5e6d4f7d4f79ebaa6fd89875a1c
Hack Reactor Students <taylor.harwin@gmail.com>
2014-04-26 18:49:45 -0700

    Complete project, refactored to pseudoclassical

--- watchout.js
+++ watchout.js
@@ -1,40 +1,37 @@
-// start slingin' some d3 here.
-
-var gameBoard = d3.select('body').append('svg').attr('class','gameBoard');
-
-var player;
-var score = 0;
-var highScore = 0;
-var collisionCount = 0;
-var settings = {
-  boardWidth: 800,
-  boardHeight: 800,
-  duration: 1000
+var Watchout = function(){
+  this.gameBoard = d3.select('body').append('svg').attr('class','gameBoard');
+  this.player;
+  this.enemies;
+  this.score = 0;
+  this.highScore = 0;
+  this.collisionCount = 0;
+  this.settings = {
+    boardWidth: 800,
+    boardHeight: 800,
+    duration: 1000
+  };
 };
 
-var drag = d3.behavior.drag()
-    .on("drag", dragmove);
+Watchout.prototype.createPlayer = function(color){
 
-function dragmove() {
-  var x = d3.event.x;
-  var y = d3.event.y;
+  var dragMove = function() {
+    var x = d3.event.x;
+    var y = d3.event.y;
+    if(y < 880 && x < 880 && x > 20 && y > 20){
+      d3.select(this).attr('x', x).attr('y', y);
+    }
+  };
 
-  // keep player within boarders of game board
-  if(y < 880 && x < 880 && x > 20 && y > 20){
-    d3.select(this).attr('x', x).attr('y', y);
-  }
-}
-
-var createPlayer = function(color){
+  var drag = d3.behavior.drag().on("drag", dragMove);
 
   // caculate center of board [x, y]
-  var rect = gameBoard.selectAll('rect').data([1]);
+  var rect = this.gameBoard.selectAll('rect').data([1]);
 
   rect.enter()
       .append('svg:rect')
       .attr('class', 'player')
-      .attr('x', (settings.boardWidth / 2))
-      .attr('y', (settings.boardHeight / 2))
+      .attr('x', (this.settings.boardWidth / 2))
+      .attr('y', (this.settings.boardHeight / 2))
       .attr('width', 20)
       .attr('height', 20)
       .attr('fill', color)
@@ -43,83 +40,76 @@ var createPlayer = function(color){
   return rect;
 };
 
-var enemies = [];
+Watchout.prototype.rand = function(n){ return (Math.random() * n) | 0; };
 
+Watchout.prototype.createEnemies = function(){
+  var enemyData = [1,2,3,4,5,6,7,8,9,10];
+  var self = this;
 
-var rand = function(n){ return (Math.random() * n) | 0; };
-
-var enemyData = [1,2,3,4,5,6,7,8,9,10];
-var enemies ;
-
-var createEnemies = function(){
-  // loop from 0 to enemyCount
-  enemies = gameBoard.selectAll('circle')
+  this.enemies = this.gameBoard.selectAll('circle')
               .data(enemyData);
 
-  enemies.enter()
-      .append("svg:circle")
+  this.enemies.enter()
+      .append('svg:circle')
       .attr('r','10')
       .attr('fill', 'yellow');
 
-  enemies.each(function(){
-    d3.select(this).attr('cx', rand(settings.boardWidth))
-      .attr('cy', rand(settings.boardHeight));
+  this.enemies.each(function(){
+    d3.select(this).attr('cx', self.rand(self.settings.boardWidth))
+      .attr('cy', self.rand(self.settings.boardHeight));
   });
 };
 
-// create function .move taking x and y for new enemy location
-// vars enemy, x, y
-var moveEnemies = function(enemies){
-  enemies.transition()
-    .duration(settings.duration)
-    .attr('cx', rand(settings.boardWidth))
-    .attr('cy', rand(settings.boardHeight))
+Watchout.prototype.moveEnemies = function(elements){
+  var self = this;
+
+  elements.transition()
+    .duration(this.settings.duration)
+    .attr('cx', this.rand(this.settings.boardWidth))
+    .attr('cy', this.rand(this.settings.boardHeight))
     .each('end', function(){
-      moveEnemies(d3.select(this));
+      self.moveEnemies(d3.select(this));
     });
 };
 
-var detectCollision = function(player, enemies){
-  enemies.each(function(){
+Watchout.prototype.detectCollision = function(){
+  var self = this;
 
-    var xDis = Math.abs(+d3.select(this).attr("cx") - +player.attr("x"));
-    var yDis = Math.abs(+d3.select(this).attr("cy") - +player.attr("y"));
+  this.enemies.each(function(){
+
+    var xDis = Math.abs(+d3.select(this).attr('cx') - +self.player.attr('x'));
+    var yDis = Math.abs(+d3.select(this).attr('cy') - +self.player.attr('y'));
 
     if(xDis <= 15 && yDis <= 15){
 
-      if (highScore === 0 || score > highScore){
-        highScore = score;
-        d3.select('.high span').text(highScore);
+      if (self.highScore === 0 || self.score > self.highScore){
+        self.highScore = self.score;
+        d3.select('.high span').text(self.highScore);
       }
 
-      score = 0;
-      collisionCount++;
+      self.score = 0;
+      self.collisionCount++;
 
-      d3.select('.collisions span').text(collisionCount);
+      d3.select('.collisions span').text(self.collisionCount);
     }
   });
 };
 
-var startGame = function(){
-  player = createPlayer('red');
-  createEnemies();
-  moveEnemies(enemies);
-
+Watchout.prototype.startGame = function(){
+  this.player = this.createPlayer('red');
+  this.createEnemies();
+  this.moveEnemies(this.enemies);
+  var self = this;
 
   d3.timer(function(){
-    detectCollision(player, enemies);
+    self.detectCollision(self.player, self.enemies);
   });
 
-
-
-  // setInterval(function(){
-  //   detectCollision(player, enemies);
-  // }, 15);
   setInterval(function(){
-    score++;
-    d3.select(".current span").text(score);
+    self.score++;
+    d3.select(".current span").text(self.score);
   }, 100);
 };
 
-startGame();
-
+var game = new Watchout();
+game.startGame();
